eae85679ef5b72aed586fcc46bd951ed6bccf9b6
geonheeCho <ghc@acts2nine.com>
2021-09-07 22:03:04 +0900

    메인로비, 프리셋 컴포넌트

--- Assets/0_HeroDefense/Scripts/UI/MainPage.cs
+++ Assets/0_HeroDefense/Scripts/UI/MainPage.cs
@@ -1,44 +1,145 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Hero_Manager;
-public class MainPage : PageInfo
+public class MainPage : MonoBehaviour
 {
-    public override void ViewInitEvent()
+    public GameObject obj_Option;//옵션
+    public GameObject obj_PvpModeStart;//pvp모드
+    public GameObject obj_CollaborationModeStart;//협동모드
+    public GameObject obj_SpecialModeStart;//스페셜모드
+    public GameObject obj_BattlePass;//배틀패스
+    public GameObject obj_UserProfile;//유저프로필
+    public GameObject ojb_DailyMission;//일일미션
+    public GameObject obj_CardBox;//카드박스
+    public GameObject obj_Fame;//명성
+    public GameObject obj_Store;//상점
+    public GameObject obj_Configuration;//구성
+    public GameObject obj_Main;//메인
+    public GameObject obj_Spetical;//스페셜
+
+
+	#region 컨텐츠 버튼이벤트
+	//유저프로필
+	public void OnClick_UserProfile()
     {
-        base.ViewInitEvent();
+        if (!obj_UserProfile.activeSelf)
+            obj_UserProfile.SetActive(true);
+        else
+            obj_UserProfile.SetActive(false);
     }
 
-    public override IEnumerator R_ViewOpenCoroutineEvent()
-    {
-        return base.R_ViewOpenCoroutineEvent();
 
+    //옵션
+    public void OnClick_Option() 
+    {
+        if (!obj_Option.activeSelf)
+            obj_Option.SetActive(true);
+        else
+            obj_Option.SetActive(false);
     }
 
-    public override IEnumerator R_ViewReOpenCoroutineEvent()
+    //카드상자
+    public void OnClick_obj_CardBox()
     {
-        return base.R_ViewReOpenCoroutineEvent();
-
+        if (!obj_CardBox.activeSelf)
+            obj_CardBox.SetActive(true);
+        else
+            obj_CardBox.SetActive(false);
     }
 
-    public override void OnClick_PrevPage()
+    //일일 미션
+    public void OnClick_DailyMission() 
     {
-        base.OnClick_PrevPage();
+        if (!ojb_DailyMission.activeSelf)
+            ojb_DailyMission.SetActive(true);
+        else
+            ojb_DailyMission.SetActive(false);
     }
 
 
+    //명성
+    public void OnClick_Fame() 
+    {
+        if (!obj_Fame.activeSelf)
+            obj_Fame.SetActive(true);
+        else
+            obj_Fame.SetActive(false);
+    }
+
+    //배틀패스
+    public void OnClick_BattlePass()
+    {
+        if (!obj_BattlePass.activeSelf)
+            obj_BattlePass.SetActive(true);
+        else
+            obj_BattlePass.SetActive(false);
+    }
+	#endregion
+
+	#region 바텀 버튼이벤트
+	public void OnClick_Store()
+    {
+        if (!obj_Store.activeSelf)
+            obj_Store.SetActive(true);
+        else
+            obj_Store.SetActive(false);
+    }
+
+    public void OnClick_Configuration()
+    {
+        if (!obj_Configuration.activeSelf)
+            obj_Configuration.SetActive(true);
+        else
+            obj_Configuration.SetActive(false);
+    }
+
+    public void OnClick_Main()
+    {
+        if (!obj_Main.activeSelf)
+            obj_Main.SetActive(true);
+        else
+            obj_Main.SetActive(false);
+    }
+
+    public void OnClick_Spetical()
+    {
+        if (!obj_Spetical.activeSelf)
+            obj_Spetical.SetActive(true);
+        else
+            obj_Spetical.SetActive(false);
+    }
+
+	#endregion
+
+    #region 게임모드 버튼이벤트
     public void OnClick_PvpModeStart() 
     {
-
+        if (!obj_PvpModeStart.activeSelf)
+            obj_PvpModeStart.SetActive(true);
+        else
+            obj_PvpModeStart.SetActive(false);
     }
 
     public void OnClick_CollaborationModeStart() 
     {
-
+        if (!obj_CollaborationModeStart.activeSelf)
+            obj_CollaborationModeStart.SetActive(true);
+        else
+            obj_CollaborationModeStart.SetActive(false);
     }
 
     public void OnClick_SpecialModeStart() 
     {
+        if (!obj_SpecialModeStart.activeSelf)
+            obj_SpecialModeStart.SetActive(true);
+        else
+            obj_SpecialModeStart.SetActive(false);
+    }
+    #endregion
 
+
+    public void OnClick_Close()
+    {
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/0_HeroDefense/Scripts/UI/Main_Choice.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Choice : MonoBehaviour
+{
+    public List<GameObject> obj_PresetBox;//5개의 프리셋이 담겨있는 5개의 프리셋박스
+    public List<GameObject> obj_PresetList;//5개의 프리셋
+    public List<GameObject> obj_HeroList;//5개의 프리셋
+
+    //선택된 프리셋
+    public void OnClick_Preset(int _index) 
+    {
+        switch (_index) 
+        {
+            case 0:
+                obj_PresetList[0].SetActive(true);
+                Debug.Log("0번 프리셋 활성화");
+                break;
+            case 1:
+                obj_PresetList[1].SetActive(true);
+                Debug.Log("1번 프리셋 활성화");
+                break;            
+            case 2:
+                obj_PresetList[2].SetActive(true);
+                Debug.Log("2번 프리셋 활성화");
+                break;            
+            case 3:
+                obj_PresetList[3].SetActive(true);
+                Debug.Log("3번 프리셋 활성화");
+                break;            
+            case 4:
+                obj_PresetList[4].SetActive(true);
+                Debug.Log("4번 프리셋 활성화");
+                break;
+            default:
+                break;
+        }
+    }
+
+    //선택된 초인 상세설명
+    public void OnClick_Hero(int _index) 
+    {
+        switch (_index)
+        {
+            case 0:
+                Debug.Log("0번 초인 설명");
+                break;
+            case 1:
+                Debug.Log("1번 초인 설명");
+                break;
+            case 2:
+                Debug.Log("2번 초인 설명");
+                break;
+            case 3:
+                Debug.Log("3번 초인 설명");
+                break;
+            case 4:
+                Debug.Log("4번 초인 설명");
+                break;
+            default:
+                break;
+        }
+    }
+}
